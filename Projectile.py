10062ff4af660e0ea1cea91c4f9aa8369dff28db
Isabelle Early <20isabelle.early@boiseschools.net>
2020-01-28 13:07:01 -0700

    shoot() method, various accessor methods, etc.

--- Projectile.py
+++ Projectile.py
@@ -2,9 +2,23 @@
 # Created by
 
 # Defines a projectile
+import pygame, sys, random
+
+WINDOW = 500  # this is temporary
+
+
 class Projectile:
     def __init__(self, t):  # t determines type of projectile
         self.type = t
+        if self.type == 1:
+            self.IMG = pygame.transform.scale(pygame.image.load('projectile1.png'), (WINDOW * 0.01, WINDOW * 0.01))
+            self.damage = 3
+
+    def getIMG(self):
+        return self.IMG
+
+    def get_damage(self):
+        return self.damage
 
     def tick(self, dt):
         pass
--- Tower.py
+++ Tower.py
@@ -2,17 +2,35 @@
 # Created by Isabelle Early
 
 from Projectile import Projectile
+from Enemy import Enemy
+import pygame, sys, random
+import math
+import data
 
-WINDOW = 500  #this is temporary
+WINDOW = 500  # this is temporary
 # Defines a tower
+
 class Tower:
     def __init__(self, t, x, y):  # t is a variable determining the type of tower
         self.type = t
         self.pos = (x, y)
-        self.projectile = Projectile(self.type)
-        if self.type == 1 :
-            self.IMG = pygame.transform.scale(pygame.image.load('tower1.png'), (WINDOW*0.05, WINDOW*0.05 ))
+        if self.type == 1:
+            self.IMG = data.resize(pygame.image.load('tower1.png'))
             self.range = 0.1*WINDOW  # radius
+
+    def shoot(self, enemy):  # given an enemy, shoots at them
+        projectile = Projectile(self.type)
+        pos = enemy.getPos()
+        if self.withinRange(pos[0], pos[1]):
+            enemy.hit(projectile.getDamage())
+            # probably add animation later
+
+    def getIMG(self):
+        return self.IMG
+
+    def getAngle(self, x, y):
+        ratio = (self.pos[y] - y) / (self.pos[0] - x)
+        return math.atan(ratio)
 
     def withinRange(self, x, y):  # given a position, returns whether that position is within range
         xVal = self.pos[0] - x
