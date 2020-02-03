6aa0db909664b41395ee48a8a2c23b85a3a1a985
Aaron <20aaron.orenstein@boiseschools.net>
2020-02-02 17:32:50 -0700

    Implemented saving created levels and level selection screen. We can now create and open individual levels!

--- run.py
+++ run.py
@@ -1,23 +1,35 @@
 # Created on 27 January 2020
 # Created by
 
+from os.path import isfile
 import math
 import pygame as pg
 from pygame.locals import *
 import data
-import LevelReader as reader
+from LevelReader import *
+from LevelDriver import LevelDriver
 
 pg.init()
-
+data.lvlDriver = LevelDriver()
 pg.display.set_mode((data.MIN_W, data.MIN_W), RESIZABLE)
 
 
-# Start playing a new level
-def start_level():
-    pass
+# Runs current level
+def run_level():
+    time = pg.time.get_ticks()
+    while True:
+        dt = pg.time.get_ticks() - time
+        time += dt
+        for e in pg.event.get():
+            if e.type == QUIT:
+                exit(0)
+            elif e.type == VIDEORESIZE:
+                data.resize(e.w, e.h)
+        data.lvlDriver.tick(dt)
+        pg.display.flip()
 
 
-# Opens main screen
+# Runs main screen
 def main_screen():
     new, choose = 1, 0
     rects = [None, None]
@@ -47,24 +59,99 @@ def main_screen():
             elif e.type == MOUSEBUTTONUP and e.button == BUTTON_LEFT:
                 pos = pg.mouse.get_pos()
                 if rects[choose].collidepoint(*pos):
-                    return choose_level()
+                    choose_level()
+                    run_level()
                 elif rects[new].collidepoint(*pos):
                     new_level()
                     draw()
         pg.display.flip()
 
 
-# Opens choose level screen
+# Runs level selection screen
 def choose_level():
-    print("Choose Level")
+    # Load all levels
+    levels = []
+    if isfile("custom_levels.bin"):
+        with open("custom_levels.bin", 'rb') as file:
+            file_data = file.read()
+        # Loop through the data
+        while len(file_data) > 0:
+            # Get the number of paths for this level
+            num_paths = int.from_bytes(file_data[0:1], byteorder)
+            byte_pos = 1
+            # Get data for each path
+            for i in range(num_paths):
+                # Make sure the path data is there
+                if len(file_data) == 0:
+                    print("Missing path data")
+                    break
+                else:
+                    # Get path type
+                    path_type = int.from_bytes(file_data[byte_pos:byte_pos + 1], byteorder)
+                    byte_pos += 1
+                    # Make sure path type is valid
+                    if path_type in constructors.keys():
+                        # Get path data and make sure it exists
+                        num_bytes = constructors[path_type]().num_bytes
+                        if len(file_data) < num_bytes:
+                            print("Missing path data")
+                            break
+                        else:
+                            byte_pos += num_bytes
+                    else:
+                        print("Unknown path type")
+                        break
+            levels.append(file_data[1:byte_pos])
+            file_data = file_data[byte_pos:]
+
+    row_len = 10
+
+    def draw():
+        d = pg.display.get_surface()
+        d.fill((0, 0, 0))
+        item_w = data.screen_w // row_len
+        back_img = pg.transform.scale(pg.image.load("back.png"), (item_w, item_w))
+        digit_w = item_w // 5
+        font = data.get_scaled_font(digit_w * 3, digit_w * 3, "0")
+        numbers = []
+        for num in range(9):
+            numbers.append(font.render(str(num), 1, (255, 255, 255)))
+        for j in range(len(levels)):
+            row, col = j // row_len, j % row_len
+            d.blit(back_img, (col * item_w, row * item_w, row_len, row_len))
+            j += 1
+            digits = len(str(j))
+            for k, val in enumerate(str(j)):
+                center = [int(item_w * (col + .5)), int(item_w * (row + .5))]
+                if digits == 2:
+                    center[0] += k - 1.5
+                elif digits == 3:
+                    center[0] += (k - 2) * 1.5
+                d.blit(numbers[int(val)], numbers[int(val)].get_rect(center=center))
+
+    draw()
+    while True:
+        for e in pg.event.get():
+            if e.type == QUIT:
+                exit(0)
+            elif e.type == VIDEORESIZE:
+                data.resize(e.w, e.h)
+                draw()
+            elif e.type == MOUSEBUTTONUP and e.button == BUTTON_LEFT:
+                pos = data.get_mouse_pos()
+                item_w = data.screen_w // row_len
+                idx = (pos[0] // item_w) + (pos[1] // item_w) * 10
+                if idx < len(levels):
+                    data.lvlDriver.lr.load_from_bytes(levels[idx])
+                    return
+        pg.display.flip()
 
 
-# TODO: Close & save level
-# Opens create a level screen
+# Runs level creator
 def new_level():
     # Start with spots for start and end
     paths = []
-    selected, current = "Start", reader.Start()
+    selected, current = "Start", Start()
     types = ["Start", "Line", "Circle", "Save & Exit"]
     rect = pg.Rect(0, 0, 0, 0)
 
@@ -73,7 +160,8 @@ def new_level():
         side_w = data.screen_w // 5
         rect.topleft = [side_w, side_w // 2]
         rect.size = [data.screen_w - side_w] * 2
-        draw_editor()
+        pg.display.get_surface().blit(draw_paths(rect, paths + [current]),
+                                      (rect.x + data.off_x, rect.y + data.off_y))
         draw_options()
 
     def draw_options():
@@ -89,45 +177,6 @@ def new_level():
             if t_ == selected:
                 pg.draw.rect(d, (175, 175, 0), (data.off_x, i * item_h + data.off_y, side_w, item_h), 5)
 
-    def draw_editor():
-        d = pg.display.get_surface()
-        s = pg.Surface(rect.size)
-        s.fill((0, 175, 0))
-        w = rect.w // 40
-        for p in paths + [current]:
-            if p.idx == reader.LINE:
-                pos_i = [int(p.start[0] * rect.w), int(p.start[1] * rect.h)]
-                pos_f = [int(p.end[0] * rect.w), int(p.end[1] * rect.h)]
-                pg.draw.line(s, (255, 255, 255), pos_i, pos_f, w)
-                pg.draw.rect(s, (255, 255, 255), (pos_i[0] - w / 2, pos_i[1] - w // 2, w, w))
-                pg.draw.rect(s, (255, 255, 255), (pos_f[0] - w / 2, pos_f[1] - w // 2, w, w))
-            elif p.idx == reader.CIRCLE:
-                c = [int(p.center[0] * rect.w), int(p.center[1] * rect.h)]
-                rad = int(p.radius * rect.w)
-                # Get theta range
-                d_theta = p.theta_f - p.theta_i
-                sign = math.copysign(1, d_theta)
-                d_theta = abs(d_theta)
-                # Break theta range into full circles
-                loop = -1
-                while d_theta >= data.TWO_PI:
-                    loop += 1
-                    d_theta -= data.TWO_PI
-                # Draw the highest full circle
-                if loop >= 0:
-                    pg.draw.circle(s, p.COLORS[loop], c, rad, min(w, rad))
-                # Draw the other sections
-                if d_theta > 0 and loop < len(p.COLORS) - 1:
-                    top_left = [c[0] - rad, c[1] - rad]
-                    thetas = [p.theta_i, p.theta_i + d_theta * sign]
-                    theta_min, theta_max = min(thetas), max(thetas)
-                    pg.draw.arc(s, p.COLORS[loop + 1], (*top_left, rad * 2, rad * 2), theta_min,
-                                theta_max, min(w, rad))
-            elif p.idx == reader.START:
-                pos = [int(p.pos[0] * rect.w), int(p.pos[1] * rect.h)]
-                pg.draw.circle(s, (0, 200, 200), pos, w)
-        d.blit(s, (rect.x + data.off_x, rect.y + data.off_y))
-
     draw()
 
     mode = 0
@@ -142,47 +191,50 @@ def new_level():
                 pos = data.get_mouse_pos()
                 if rect.collidepoint(*pos):
                     pos = [(pos[0] - rect.x) / rect.w, (pos[1] - rect.y) / rect.h]
-                    if current.idx == reader.START:
+                    if current.idx == START:
                         current.pos = pos
                         paths = [current]
-                        current = reader.Start()
-                    elif current.idx == reader.LINE:
+                        current = Start()
+                    elif current.idx == LINE:
                         paths.append(current)
-                        current = reader.Line(start=pos)
-                    elif current.idx == reader.CIRCLE:
+                        current = Line(start=pos)
+                    elif current.idx == CIRCLE:
                         if mode == 0:
                             mode = 1
                         else:
                             paths.append(current)
-                            current = reader.Circle()
+                            current = Circle()
                             mode = 0
                 else:
                     idx = pos[1] * len(types) // data.screen_w
                     if idx >= len(types) - 1:
-                        print("Save")
+                        with open("custom_levels.bin", "ab+") as file:
+                            file.write(len(paths).to_bytes(1, byteorder))
+                            for p in paths:
+                                file.write(p.to_bytes())
                         return
                     elif selected != types[idx] and len(paths) > 0:
                         selected = types[idx]
                         draw_options()
                         if selected == "Start":
-                            current = reader.Start()
+                            current = Start()
                         elif selected == "Line":
-                            current = reader.Line(start=paths[-1].get_end())
+                            current = Line(start=paths[-1].get_end())
                         elif selected == "Circle":
-                            current = reader.Circle()
+                            current = Circle()
                         mode = 0
             elif e.type == MOUSEMOTION:
                 pos = data.get_mouse_pos()
                 pos = [(pos[0] - rect.x) / rect.w, (pos[1] - rect.y) / rect.h]
                 pos_ = [min(max(i, 0), 1) for i in pos]
-                if current.idx == reader.START:
+                if current.idx == START:
                     current.pos = pos_
-                elif current.idx == reader.LINE:
+                elif current.idx == LINE:
                     current.end = pos_
                     for i in range(2):
                         if abs(current.end[i] - current.start[i]) < .025:
                             current.end[i] = current.start[i]
-                elif current.idx == reader.CIRCLE:
+                elif current.idx == CIRCLE:
                     data.TWO_PI = 2 * math.pi
                     if mode == 0:
                         # Get end of last segment
@@ -233,8 +285,8 @@ def new_level():
                             current.theta_f = ten_pi + current.theta_i
                         elif current.theta_f < -ten_pi + current.theta_i:
                             current.theta_f = -ten_pi + current.theta_i
-                draw_editor()
-
+                pg.display.get_surface().blit(draw_paths(rect, paths + [current]),
+                                              (rect.x + data.off_x, rect.y + data.off_y))
         pg.display.flip()
 
 
--- test.py
+++ test.py
@@ -1,3 +1,1 @@
-d = v = 0
-r = 1
-print(d, v, r)
+print(999)
