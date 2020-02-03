c62fb7c046ac9ac9cff159ebd06d2a05c2023463
nicol691 <54330056+nicol691@users.noreply.github.com>
2020-02-02 22:48:20 -0700

    Third Commit

--- Enemy.py
+++ Enemy.py
@@ -2,15 +2,21 @@
 # Created by Nicole Brown
 
 import pygame
+import data
 
 
 #  Defines an enemy
 class Enemy:
-    def __init__(self, image, x, y, strength):
+    def __init__(self, x, y, strength):
         self.x = x   # x position
         self.y = y   # y position
         self.strength = strength
-        self.image = pygame.image.load(image)
+        if self.strength == 1:
+            self.image = data.resize(pygame.image.load("enemy1.png"))
+        elif self.strength == 2:
+            self.image = data.resize(pygame.image.load("enemy2.png"))
+        elif self.strength == 3:
+            self.image = data.resize(pygame.image.load("enemy3.png"))
 
     #  This method reduces the strength by the damage amount
     def hit(self, damage):
@@ -23,13 +29,10 @@ class Enemy:
         self.x = x   # x position
         self.y = y   # y position
 
-    # Draw
-    def draw(self, display, w, h):
-        if self.strength > 0:
-            pass
-            resize_image = pygame.transform.scale(self.image, (w, h))
-            display.blit(resize_image, (self.x, self.y))
-
     # Gets position
     def getpos(self):
         return self.x, self.y
+
+    # Returns image
+    def return_image(self):
+        return self.image
