6e19abfeb348bc43e51869ce0b33420062a5b676
nicol691 <54330056+nicol691@users.noreply.github.com>
2020-02-11 12:58:24 -0700

    Added enemy classes

--- Enemy.py
+++ Enemy.py
@@ -2,7 +2,9 @@
 # Created by Nicole Brown
 
 import pygame
+import data
 
+WINDOW = data.screen_w
 
 #  Defines an enemy
 class Enemy:
@@ -10,19 +12,19 @@ class Enemy:
         self.pos = (x, y)
         self.strength = strength
         if self.strength == 1:
-            self.image = pygame.transform.scale(pygame.image.load("enemy1.png"))
+            self.image = pygame.transform.scale(pygame.image.load("enemy1.png"), (int(WINDOW * 0.05), int(WINDOW * 0.05)))
             self.v = 5  # 5% of screen width per second (20 seconds to move across the screen)
         elif self.strength == 2:
-            self.image = pygame.transform.scale(pygame.image.load("enemy2.png"))
+            self.image = pygame.transform.scale(pygame.image.load("enemy2.png"), (int(WINDOW * 0.05), int(WINDOW * 0.05)))
             self.v = 10  # 10% of screen width per second (10 seconds to move across the screen)
         elif self.strength == 3:
-            self.image = pygame.transform.scale(pygame.image.load("enemy3.png"))
+            self.image = pygame.transform.scale(pygame.image.load("enemy3.png"), (int(WINDOW * 0.05), int(WINDOW * 0.05)))
             self.v = 20  # 20% of screen width per second (5 seconds to move across the screen)
         elif self.strength == 4:
-            self.image = pygame.transform.scale(pygame.image.load("enemy4.png"))
+            self.image = pygame.transform.scale(pygame.image.load("enemy4.png"), (int(WINDOW * 0.05), int(WINDOW * 0.05)))
             self.v = 30  # 30% of screen width per second
         elif self.strength == 5:
-            self.image = pygame.transform.scale(pygame.image.load("enemy5.png"))
+            self.image = pygame.transform.scale(pygame.image.load("enemy5.png"), (int(WINDOW * 0.05), int(WINDOW * 0.05)))
             self.v = 40  # 40% of screen width per second
 
     #  This method reduces the strength by the damage amount
