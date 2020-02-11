3b1140978b3164c19fed02d329a33f849fe67add
Isabelle Early <20isabelle.early@boiseschools.net>
2020-02-11 12:37:48 -0700

    shoot() method, various accessor methods, etc.

--- Tower.py
+++ Tower.py
@@ -22,7 +22,7 @@ class Tower:
         pass
 
     def getAngle(self, x, y):
-        ratio = (self.pos[0] - y) / (self.pos[0] - x)
+        ratio = (self.pos[1] - y) / (self.pos[0] - x)
         return math.atan(ratio)
 
     def withinRange(self, x, y, r):
