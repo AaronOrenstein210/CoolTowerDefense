c83503f0c36aa40aec918a7229520d6462497468
Aaron <20aaron.orenstein@boiseschools.net>
2020-02-28 22:30:07 -0700

    Updated .gitignore

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-test = "thre"
+class A:
+    var = 1
 
-print(test[4:5])
+    def __init__(self, inc):
+        self.inc = inc
+
+    def increment(self):
+        self.var += self.inc
+
+    def new_b(self):
+        b = self.B()
+        b.out()
+
+    class B:
+        def __init__(self):
+            self.val = A.var
+
+        def out(self):
+            print(self.val)
+
+
+a = A(3)
+a.increment()
+a.new_b()
